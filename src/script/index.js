d324fa1b1200499ebdbb5a0a0a50b0d70cdda34f
usnabeel1 <usmmannabeel39@yahoo.com>
2020-11-08 23:32:12 +0200

    additional content

--- src/script/index.js
+++ src/script/index.js
@@ -1,2 +1,5 @@
 const $like = document.querySelector("#like");
 $like.addEventListener("click",()=>($like.style.color = "red"));
+var d = new Date();
+document.getElementById("demo").innerHTML= d;
+
